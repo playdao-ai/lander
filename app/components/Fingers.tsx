1c6dd8c3f1ff59daf96f24eebfd8dd93d2b70b4c
pollen-ys <yury.sidorov@pollenmobile.io>
2023-04-12 11:20:19 -0700

    update pill mesh geometry physics bindings

--- app/components/Fingers.tsx
+++ app/components/Fingers.tsx
@@ -36,11 +36,6 @@ Math.step = function (x, y) {
 // lipids will also merge into 1 lipid if the width is too small
 
 
-class Lipid {
-	constructor() {
-
-	}
-}
 
 
 let i_ray = new THREE.Ray()
@@ -52,6 +47,30 @@ function findIntersection(v1, n_1, v2, n_2) {
 	// console.log(v2_ray.intersectPlane(v1_plane, new THREE.Vector3()))
 	return i_ray.intersectPlane(i_plane, new THREE.Vector3());
 }
+
+
+
+function findExternalTangents(cv_1, cr_1, cv_2, cr_2) {
+	const dx = cv_2.x - cv_1.x;
+	const dy = cv_2.y - cv_1.y;
+	const dist = Math.sqrt(dx * dx + dy * dy);
+
+	const a = Math.atan2(dy, dx);
+
+	const r1 = cr_1;
+	const r2 = cr_2;
+
+	const angle1 = Math.acos((r1 - r2) / dist) || 0;
+
+	let t1_a = new THREE.Vector3(cv_1.x + r1 * Math.cos(a + angle1), cv_1.y + r1 * Math.sin(a + angle1), 0)
+	let t1_b = new THREE.Vector3(cv_2.x + r2 * Math.cos(a + angle1), cv_2.y + r2 * Math.sin(a + angle1), 0)
+
+
+	return [t1_a, t1_b]
+
+}
+
+//project ray 
 
 // function findIntersection(A, vectorA, B, vectorB) {
 // 	const rayA = new THREE.Ray(A, vectorA);
@@ -123,12 +142,13 @@ export class Cell {
 	private pills: THREE.Mesh[] = [];
 	private world: Matter.World;
 	private engine: Matter.Engine;
+	private scene: THREE.Scene;
 
 
 
-
-	constructor(settings, camera, renderer, world, engine) {
+	constructor(settings, camera, renderer, world, engine, scene) {
 		this.root = new THREE.Object3D()
+		this.scene = scene
 		this.settings = settings
 		this.camera = camera
 		this.renderer = renderer
@@ -145,6 +165,7 @@ export class Cell {
 	buildDragControls() {
 		let joint_drag_controls = new DragControls(this.joint_handles, this.camera, this.renderer.domElement);
 		joint_drag_controls.addEventListener('drag', function (event) {
+			event.object.position.z = 0
 			let joint_index = event.object._index
 			this.joints_buffer[joint_index * 3 + 0] = event.object.position.x
 			this.joints_buffer[joint_index * 3 + 1] = event.object.position.y
@@ -233,9 +254,13 @@ export class Cell {
 	}
 
 	updatePillConstraints(pill) {
-		let { midpoint, base_radius, normal, max_height_point } = this.getPillVertices(pill)
+		let { v0, v1, v2, v3, midpoint, base_radius, normal, tip_position, max_height_point } = this.getPillVertices(pill)
+
+
 		pill.base_target_point.copy(midpoint)
 
+		let basepoint = new THREE.Vector3(pill.matter_base.position.x, pill.matter_base.position.y, 0)
+		let tippoint = new THREE.Vector3(pill.matter_tip.position.x, pill.matter_tip.position.y, 0)
 
 		pill.base_radius = Math.lerp(pill.base_radius, base_radius, 0.1)
 		pill.tip_radius = Math.lerp(pill.tip_radius, base_radius / 2, 0.1)
@@ -243,9 +268,77 @@ export class Cell {
 		pill.matter_base.circleRadius = pill.base_radius
 		pill.matter_tip.circleRadius = 0.00
 
-
+		pill.cylinder.position.copy(basepoint).add(normal.clone().multiplyScalar(pill.base_radius))
 
 		pill.tip_target_point.copy(midpoint).add(normal.clone().multiplyScalar(this.pill_height_buffer[pill._index]))
+
+
+		let vc = pill.cylinder.geometry.attributes.position.array.length / 3
+		let sides = pill.sides
+
+		let [p1, p2] = findExternalTangents(tippoint, pill.tip_radius, basepoint, pill.base_radius)
+
+
+		let basepoint_normal = new THREE.Vector3().subVectors(tippoint, basepoint).normalize()
+
+		const plane2 = new THREE.Plane().setFromNormalAndCoplanarPoint(basepoint_normal, basepoint);
+
+		const plane = new THREE.Plane().setFromNormalAndCoplanarPoint(findScaledNormal(basepoint, basepoint.clone().add(basepoint_normal)), basepoint);
+		// Calculate the distance from the plane to point B
+		const r = plane.distanceToPoint(p1);
+		let r2 = plane2.distanceToPoint(p1);
+
+
+		const r3 = plane.distanceToPoint(p2);
+		let r4 = plane2.distanceToPoint(p2);
+
+
+		for (let i = 0; i < vc / 2; i++) {
+			pill.cylinder.geometry.attributes.position.array[i * 3 + 0] = Math.cos((Math.PI * 2 / sides) * i) * r
+			pill.cylinder.geometry.attributes.position.array[i * 3 + 2] = Math.sin((Math.PI * 2 / sides) * i) * r
+			pill.cylinder.geometry.attributes.position.array[i * 3 + 1] = r2
+		}
+
+
+		for (let i = vc / 2; i < vc; i++) {
+			pill.cylinder.geometry.attributes.position.array[i * 3 + 0] = Math.cos((Math.PI * 2 / sides) * i) * r3
+			pill.cylinder.geometry.attributes.position.array[i * 3 + 2] = Math.sin((Math.PI * 2 / sides) * i) * r3
+			pill.cylinder.geometry.attributes.position.array[i * 3 + 1] = r4
+		}
+
+		let rot_y = Math.atan2(basepoint_normal.x, basepoint_normal.y)
+
+		pill.cylinder.geometry.attributes.position.needsUpdate = true
+		pill.cylinder.position.set(0, 0, 0)
+		pill.cylinder.position.z = 0
+
+		pill.cylinder.rotation.z = -rot_y
+		pill.cylinder.position.copy(basepoint)
+
+
+		//tip_tangent_offset
+
+
+		// pill.tip.position.copy(pill.matter_base.position).add(normal.multiplyScalar(tip_tangent_offset))
+		pill.tip.position.z = 0
+		pill.tip.rotation.y = rot_y
+		pill.tip.position.set(tippoint.x, tippoint.y, 0)
+		pill.tip.scale.x = pill.tip_radius
+		pill.tip.scale.y = pill.tip_radius
+		pill.tip.scale.z = pill.tip_radius
+
+		pill.base.position.z = 0
+		pill.base.rotation.y = rot_y
+		pill.base.position.set(basepoint.x, basepoint.y, 0)
+		pill.base.scale.x = pill.base_radius
+		pill.base.scale.y = pill.base_radius
+		pill.base.scale.z = pill.base_radius
+
+
+		pill.cylinder.scale.x = 1
+		pill.cylinder.scale.y = 1
+		pill.cylinder.scale.z = 1
+
 
 
 		// pill.matter_constraint_2.length = pill.base_radius + pill.tip_radius
@@ -254,7 +347,7 @@ export class Cell {
 
 
 	updatePillHelper(pill) {
-		let { v0, v1, v2, v3, normal, prev_normal, next_normal, left_normal, right_normal, midpoint, max_height_point, max_height, left_intersection_point, right_intersection_point } = this.getPillVertices(pill)
+		let { v0, v1, v2, v3, normal, base_radius, tip_position, prev_normal, next_normal, left_normal, right_normal, midpoint, max_height_point, max_height, left_intersection_point, right_intersection_point } = this.getPillVertices(pill)
 
 		let v1_line_v2 = v1.clone().add(left_normal.clone().multiplyScalar(10))
 		let v2_line_v2 = v2.clone().add(right_normal.clone().multiplyScalar(4))
@@ -262,6 +355,12 @@ export class Cell {
 		pill.helper.v1_normal_buffer.set([v1.x, v1.y, v1.z, v1_line_v2.x, v1_line_v2.y, v1_line_v2.z])
 		pill.helper.v2_normal_buffer.set([v2.x, v2.y, v2.z, v2_line_v2.x, v2_line_v2.y, v2_line_v2.z])
 		pill.helper.mid_normal_buffer.set([midpoint.x, midpoint.y, midpoint.z, max_height_point.x, max_height_point.y, max_height_point.z])
+
+
+
+
+
+
 
 		pill.helper.v1_mixed_normal_line.geometry.attributes.position.needsUpdate = true
 		pill.helper.v2_mixed_normal_line.geometry.attributes.position.needsUpdate = true
@@ -272,23 +371,33 @@ export class Cell {
 		let r = diameter
 
 
-		// pill.helper.bottom_circle.position.copy(midpoint)
-		// pill.helper.top_circle.position.copy(max_point)
-
-
 		pill.helper.bottom_circle.position.x = pill.matter_base.position.x
 		pill.helper.bottom_circle.position.y = pill.matter_base.position.y
+
+
+
+		pill.helper.bottom_circle.scale.set(pill.base_radius * 2, pill.base_radius * 2, 1)
+		pill.helper.top_circle.scale.set(pill.tip_radius * 2, pill.tip_radius * 2, 1)
 
 		pill.helper.top_circle.position.x = pill.matter_tip.position.x
 		pill.helper.top_circle.position.y = pill.matter_tip.position.y
 
-		pill.helper.bottom_circle.scale.set(pill.base_radius, pill.base_radius, 1)
-		pill.helper.top_circle.scale.set(pill.tip_radius, pill.tip_radius, 1)
-		// console.log(pill.helper.bottom_circle.position, pill.matter_base.position)
+		pill.helper.top_circle.position.copy(tip_position)
 
-		let height = Math.min(max_height, this.pill_height_buffer[pill._index])
 
-		pill.helper.height_handle.position.copy(midpoint.add(normal.clone().multiplyScalar(height)))
+
+		pill.helper.height_handle.position.copy(tip_position)
+
+		pill.top_position = pill.helper.height_handle.position
+
+		let [p1, p2] = findExternalTangents(midpoint, base_radius, tip_position, pill.tip_radius)
+
+
+
+		pill.helper.tangent_buffer.set([p1.x, p1.y, 0, p2.x, p2.y, 0])
+		pill.helper.tangent_line.geometry.attributes.position.needsUpdate = true
+
+
 	}
 
 
@@ -297,6 +406,7 @@ export class Cell {
 		let v1_normal_buffer = new Float32Array(3 * 2)
 		let v2_normal_buffer = new Float32Array(3 * 2)
 		let mid_normal_buffer = new Float32Array(3 * 2)
+		let tangent_buffer = new Float32Array(3 * 2)
 
 		let mid_normal_geometry = new THREE.BufferGeometry();
 		mid_normal_geometry.setAttribute('position', new THREE.BufferAttribute(mid_normal_buffer, 3));
@@ -307,10 +417,20 @@ export class Cell {
 		let v2_normal_geometry = new THREE.BufferGeometry();
 		v2_normal_geometry.setAttribute('position', new THREE.BufferAttribute(v2_normal_buffer, 3));
 
+		let tangent_geometry = new THREE.BufferGeometry();
+		tangent_geometry.setAttribute('position', new THREE.BufferAttribute(tangent_buffer, 3));
+
 
 		let mid_normal_line = new THREE.LineSegments(mid_normal_geometry);
+		let tangent_line = new THREE.LineSegments(tangent_geometry);
 		let v1_mixed_normal_line = new THREE.LineSegments(v1_normal_geometry);
 		let v2_mixed_normal_line = new THREE.LineSegments(v2_normal_geometry);
+
+
+		tangent_line.material.depthTest = false;
+		tangent_line.material.opacity = 0.25;
+		tangent_line.material.transparent = true;
+		tangent_line.material.color = new THREE.Color(0xff8c00);
 
 		v1_mixed_normal_line.material.depthTest = false;
 		v1_mixed_normal_line.material.opacity = 0.25;
@@ -355,6 +475,9 @@ export class Cell {
 		helper.add(mid_normal_line).add(v1_mixed_normal_line).add(v2_mixed_normal_line).add(top_circle).add(bottom_circle)
 		helper.mid_normal_line = mid_normal_line
 
+		helper.add(tangent_line)
+		helper.tangent_line = tangent_line
+
 		helper.v1_mixed_normal_line = v1_mixed_normal_line
 		helper.v2_mixed_normal_line = v2_mixed_normal_line
 
@@ -365,6 +488,8 @@ export class Cell {
 
 		helper.bottom_circle = bottom_circle
 		helper.top_circle = top_circle
+
+		helper.tangent_buffer = tangent_buffer
 
 		// helper.left_intersection = left_intersection
 		// helper.right_intersection = right_intersection
@@ -380,6 +505,8 @@ export class Cell {
 
 		return helper
 	}
+
+
 
 	buildPill(joint_index_a, joint_index_b, prev_pill) {
 
@@ -450,13 +577,46 @@ export class Cell {
 		pill.matter_base.collisionFilter.category = 0x0001;
 		pill.matter_tip.collisionFilter.category = 0x0002;
 
+		// const material = new THREE.ShaderMaterial({
+
+		// 	uniforms: {
+
+		// 		time: { value: 1.0 },
+		// 		resolution: { value: new THREE.Vector2() }
+
+		// 	},
+
+		// 	vertexShader: document.getElementById('vertexShader').textContent,
+
+		// 	fragmentShader: document.getElementById('fragmentShader').textContent
+
+		// });
+
+		let sides = 16
+		pill.sides = sides
 
 
+		// const finger_material = new THREE.MeshBasicMaterial({ color: 0xffff00 });
+		const finger_material = new THREE.MeshStandardMaterial({ color: 0xffff00 });
+		finger_material.wireframe = true;
+		finger_material.transparency = true;
+		finger_material.opacity = 0.5;
+		finger_material.isVisible = false
 
+		const tip_geometry = new THREE.SphereGeometry(1, 12, 12, 0, Math.PI, 0, Math.PI);
+		const finger_geometry = new THREE.CylinderGeometry(1, 1, 1, sides, 1, true, 0, Math.PI * 2);
+		const base_geometry = new THREE.SphereGeometry(1, 24, 24, 0, Math.PI * 2, 0, Math.PI * 2);
 
+		pill.cylinder = new THREE.Mesh(finger_geometry, finger_material);
+		pill.tip = new THREE.Mesh(tip_geometry, finger_material);
+		pill.base = new THREE.Mesh(base_geometry, finger_material);
 
+		pill.tip.rotation.x = pill.base.rotation.x = -Math.PI / 2
 
-
+		this.root.add(pill.cylinder);
+		this.root.add(pill.tip)
+		this.root.add(pill.base)
+		// console.log(pill.cylinder);
 
 		pill.base_radius = 0
 		pill.tip_radius = 0
@@ -491,6 +651,8 @@ export class Cell {
 		let joint_index_a = pill.v1_index
 		let joint_index_b = pill.v2_index
 
+
+
 		let v1 = new THREE.Vector3(this.joints_buffer[joint_index_a * 3 + 0], this.joints_buffer[joint_index_a * 3 + 1], this.joints_buffer[joint_index_a * 3 + 2])
 		let v0 = joint_index_a > 0 ? new THREE.Vector3(this.joints_buffer[(joint_index_a - 1) * 3 + 0], this.joints_buffer[(joint_index_a - 1) * 3 + 1], this.joints_buffer[(joint_index_a - 1) * 3 + 2]) : v1
 		let v2 = new THREE.Vector3(this.joints_buffer[joint_index_b * 3 + 0], this.joints_buffer[joint_index_b * 3 + 1], this.joints_buffer[joint_index_b * 3 + 2])
@@ -501,9 +663,10 @@ export class Cell {
 		let left_normal = new THREE.Vector3().lerpVectors(prev_normal, normal, 0.5).normalize()
 		let right_normal = new THREE.Vector3().lerpVectors(normal, next_normal, 0.5).normalize()
 		let midpoint = new THREE.Vector3().addVectors(v1, v2).divideScalar(2)
-		let base_radius = v1.distanceTo(v2)
+		let base_radius = v1.distanceTo(v2) / 2
 
-		let max_height = 4 //this.pill_height_buffer[pill._index]
+
+		let max_height = 10 //this.pill_height_buffer[pill._index]
 
 		var max_height_point = new THREE.Vector3()
 
@@ -539,12 +702,14 @@ export class Cell {
 		}
 
 
+
 		max_height_point.copy(midpoint).addScaledVector(normal, max_height)
 
-		return { v0, v1, v2, v3, normal, base_radius, prev_normal, next_normal, left_normal, right_normal, midpoint, max_height_point, max_height, left_intersection_point, right_intersection_point }
+		let height = Math.min(max_height, this.pill_height_buffer[pill._index])
+		let tip_position = midpoint.clone().addScaledVector(normal, height)
+
+		return { v0, v1, v2, v3, normal, base_radius, prev_normal, next_normal, left_normal, right_normal, midpoint, max_height_point, max_height, height, tip_position, left_intersection_point, right_intersection_point }
 	}
-
-
 
 }
 
@@ -573,7 +738,7 @@ export class SampleLipidScene {
 		this.height = canvas_el.clientHeight;
 		let settings = {
 			camera_rotation: false,
-			joint_count: 10,
+			joint_count: 3,
 		}
 		this.settings = settings
 		let gui = new GUI();
@@ -610,7 +775,7 @@ export class SampleLipidScene {
 
 		this.camera = new THREE.PerspectiveCamera(75, this.width / this.height, 0.1, 1000);
 		this.controls = new OrbitControls(this.camera, this.renderer.domElement);
-		console.log(this.controls)
+		// console.log(this.controls)
 
 		//move camera back
 		this.camera.position.z = 10;
@@ -644,7 +809,7 @@ export class SampleLipidScene {
 
 		window.addEventListener('resize', this.resize.bind(this));
 
-		this.cell = new Cell(this.settings, this.camera, this.renderer, this.world, this.engine)
+		this.cell = new Cell(this.settings, this.camera, this.renderer, this.world, this.engine, this.scene)
 		this.scene.add(this.cell.root)
 
 		this.animate(0)
@@ -666,6 +831,7 @@ export class SampleLipidScene {
 
 		this.controls.update();
 		this.renderer.render(this.scene, this.camera);
+		this.camera.updateProjectionMatrix()
 		this.cell.animate();
 
 		requestAnimationFrame(this.animate.bind(this));
